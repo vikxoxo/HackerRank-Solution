10ded0477a16ef8d478ccb44502a3013c94a486f
vikxoxo <41680951+vikxoxo@users.noreply.github.com>
2020-08-10 13:31:01 +0530

    Create SockMerchant.py

---
+++ Sock Merchant/SockMerchant.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count = 0
+    set = []
+    for e in ar:
+        if e in set:
+            count = count + 1
+            set.remove(e)
+        else:
+            set.append(e)
+    return count        
+
+            
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
