4b98faec3f7b46aa97c593571bfaea32f6533f0e
vikxoxo <41680951+vikxoxo@users.noreply.github.com>
2020-08-17 13:43:34 +0530

    Create Arrays: Left Rotation.py

---
+++ Arrays: Left Rotation.py
@@ -0,0 +1,37 @@
+#Arrays: Left Rotation
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    l =[]
+    if len(a)>d:
+        l = list(a[d:])
+        l.extend(a[:d])
+    else:
+        print("Constraint not satisfied")
+
+    return l 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+    print(result)
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
