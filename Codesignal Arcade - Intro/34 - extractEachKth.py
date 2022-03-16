bc808ec6a7bcb72a8441570ad48c7a73f50235f8
vikxoxo <41680951+vikxoxo@users.noreply.github.com>
2022-03-16 15:59:06 +0530

    Update 34 - extractEachKth.py

--- Codesignal Arcade - Intro/34 - extractEachKth.py
+++ Codesignal Arcade - Intro/34 - extractEachKth.py
@@ -5,6 +5,9 @@ Example
 
 For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
 solution(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+
+Read this
+https://www.geeksforgeeks.org/python-list-slicing/
 """
 def solution(inputArray, k):
     l = []
